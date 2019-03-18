d5f1642c2b2c2a2bac693df4e5edb48d43139c0d
rque0001 <User@DESKTOP-TOTEUBD>
2019-03-19 10:26:08 +1100

    revised json parsing

--- readFile.py
+++ readFile.py
@@ -4,7 +4,6 @@ with open("harvey.json",'r',encoding='utf-8') as json_file:
     f.write("{")
     for line in json_file:
         data = json.loads(line)
-        #print(json.dumps(data,indent=4,sort_keys=True))
         newDict = {}
         newDict['created_at']=data['created_at']
         newDict['id_str']=data['id_str']
@@ -12,22 +11,14 @@ with open("harvey.json",'r',encoding='utf-8') as json_file:
         newDict['in_reply_to_user_id_str']=data['in_reply_to_user_id_str']
         newDict['in_reply_to_screen_name']=data['in_reply_to_screen_name']
         newDict['entities']=data['entities']
-        #print(json.dumps(data['user'],indent=4,sort_keys=True))
-
         newDict['user']={}
         newDict['user']['followers_count']=data['user']['followers_count']
-        
         newDict['user']['name']=data['user']['name']
-        #newDict['quoted_status_id_str']=data['quoted_status_id_str']
-        #newDict['is_quote_status']=data['is_quote_status']
         newDict['retweet_count']=data['retweet_count']
         newDict['favorite_count']=data['favorite_count']
-        newDict['full_text']=data['full_text']
-        
+        newDict['full_text']=data['full_text']        
         f.write(json.dumps(newDict))
         f.write("\n,")
-        #print(newDict)
-        #print(data['created_at'])
 
     f.write("{}}")
     f.close()

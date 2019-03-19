444787654f1251b45bc2aed74919c50c62f273c9
rque0001 <User@DESKTOP-TOTEUBD>
2019-03-19 13:18:26 +1100

    modified json parsing

--- readFile.py
+++ readFile.py
@@ -1,24 +1,30 @@
 import json
 with open("harvey.json",'r',encoding='utf-8') as json_file:
+    idDict={}
     f= open("harvey_cleaned.json","w+")
     f.write("{")
     for line in json_file:
         data = json.loads(line)
         newDict = {}
-        newDict['created_at']=data['created_at']
-        newDict['id_str']=data['id_str']
-        newDict['in_reply_to_status_id_str']=data['in_reply_to_status_id_str']
-        newDict['in_reply_to_user_id_str']=data['in_reply_to_user_id_str']
-        newDict['in_reply_to_screen_name']=data['in_reply_to_screen_name']
-        newDict['entities']=data['entities']
-        newDict['user']={}
-        newDict['user']['followers_count']=data['user']['followers_count']
-        newDict['user']['name']=data['user']['name']
-        newDict['retweet_count']=data['retweet_count']
-        newDict['favorite_count']=data['favorite_count']
-        newDict['full_text']=data['full_text']        
-        f.write(json.dumps(newDict))
-        f.write("\n,")
+        if idDict.get(data['id_str']) is None:
+            #print(idDict)
+            idDict[data['id_str']]=data['id_str']
+            f.write('"'+data['id_str']+'":')
+            newDict['created_at']=data['created_at']
+            newDict['id_str']=data['id_str']
+            newDict['in_reply_to_status_id_str']=data['in_reply_to_status_id_str']
+            newDict['in_reply_to_user_id_str']=data['in_reply_to_user_id_str']
+            newDict['in_reply_to_screen_name']=data['in_reply_to_screen_name']
+            newDict['hashtags']=data['entities']['hashtags']
+            newDict['user']={}
+            newDict['user']['id_str']=data['user']['id_str']
+            newDict['user']['followers_count']=data['user']['followers_count']
+            newDict['user']['name']=data['user']['name']
+            newDict['retweet_count']=data['retweet_count']
+            newDict['favorite_count']=data['favorite_count']
+            newDict['full_text']=data['full_text']        
+            f.write(json.dumps(newDict))
+            f.write("\n,")
 
-    f.write("{}}")
+    f.write('"last":{}}')
     f.close()

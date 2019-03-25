fb4a457abd68150df73d30da729bb30c32b11cfe
rque0001 <User@DESKTOP-TOTEUBD>
2019-03-25 20:30:00 +1100

    changed json parsing

--- readFile.py
+++ readFile.py
@@ -1,7 +1,7 @@
 import json
-with open("harvey.json",'r',encoding='utf-8') as json_file:
+with open("irma.json",'r',encoding='utf-8') as json_file:
     idDict={}
-    f= open("harvey_cleaned.json","w+")
+    f= open("irma_cleaned.json","w+")
     f.write("{")
     for line in json_file:
         data = json.loads(line)
@@ -15,11 +15,12 @@ with open("harvey.json",'r',encoding='utf-8') as json_file:
             newDict['in_reply_to_status_id_str']=data['in_reply_to_status_id_str']
             newDict['in_reply_to_user_id_str']=data['in_reply_to_user_id_str']
             newDict['in_reply_to_screen_name']=data['in_reply_to_screen_name']
-            newDict['hashtags']=data['entities']['hashtags']
-            newDict['user']={}
-            newDict['user']['id_str']=data['user']['id_str']
-            newDict['user']['followers_count']=data['user']['followers_count']
-            newDict['user']['name']=data['user']['name']
+            if len(data['entities']['hashtags'])>0:
+
+                newDict['hashtags']=data['entities']['hashtags'][0]['text']
+            newDict['user_id_str']=data['user']['id_str']
+            newDict['user_followers_count']=data['user']['followers_count']
+            newDict['user_name']=data['user']['name']
             newDict['retweet_count']=data['retweet_count']
             newDict['favorite_count']=data['favorite_count']
             newDict['full_text']=data['full_text']        

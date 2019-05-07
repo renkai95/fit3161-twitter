c7c7d8b678f42a4afeed0e6105ab04b748e668cc
rque0001 <User@DESKTOP-TOTEUBD>
2019-05-07 14:00:35 +1000

    add file

---
+++ databaseinsert.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM twitter.harvey_cleaned;
+SHOW VARIABLES LIKE "secure_file_priv";
+truncate table harvey_cleaned;
+LOAD DATA INFILE 'D:\DATA\\harvey2daysoutput.csv' 
+INTO TABLE harvey_cleaned 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '|'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;

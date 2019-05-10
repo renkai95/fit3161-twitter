ccaf0b4e698b7578a09381a71572495755f39848
rque0001 <User@DESKTOP-TOTEUBD>
2019-05-10 16:51:41 +1000

    added documentation for build-tree

--- build-tree.py
+++ build-tree.py
@@ -8,12 +8,12 @@ import numpy as np
 import sys
 
 
-def traverse(g,node,harvey_id):
+def traverse(g,node,tweet_ids):
     '''
     Recursive traversal of a python reply tree dict
     :param g: networkx graph object
     :param node: Current node name
-    :param harvey_id: dict of twitter id_str
+    :param tweet_ids: dict of twitter id_str
     Returns: Nothing
     Side effects: Adds node to graph if there exists a reply, calls itself recursively if there is a child node
     
@@ -23,107 +23,145 @@ def traverse(g,node,harvey_id):
     except KeyError:
         return
     for child in node['id_children']:
-        g.add_node(harvey_id[child['id']])
-        g.add_node(harvey_id[node['id']])
-        g.add_edge(harvey_id[child['id']],harvey_id[node['id']])
-        traverse(g,child,harvey_id)
+        g.add_node(tweet_ids[child['id']])
+        g.add_node(tweet_ids[node['id']])
+        g.add_edge(tweet_ids[child['id']],tweet_ids[node['id']])
+        traverse(g,child,tweet_ids)
 
-        
-cnx = mysql.connector.connect(user='root', password='password',
-                              host='127.0.0.1',
-                              database='twitter')
-cursor = cnx.cursor()
-harvey_id={}
-replied_to=[]
-replied_to_id=[]
-replied_inlist={}
-tree= []
 
-font = {'family' : 'normal',
-        'size'   : 1}
+def readFile(idFile,replyFile):
+    '''
+    Reads file, splits results into dicts
+    returns: dict of tweet ids, dict of all reply ids, list of tuples of reply ids
+    '''
+    tweet_ids={}
+    replied_inlist={}
+    replied_to=[]
+    with open(idFile,encoding='utf-8') as inputFile:
+        for row in inputFile:
+            tweet_ids[row.rstrip()]=row.rstrip()
+        f = open(replyFile,encoding='utf-8')
+        next(f)
+        for row in f:
+            temp=row.split(',')
+            try:
+                tweet_ids[temp[1].rstrip()]
+                replied_to.append((temp[0].rstrip(),temp[1].rstrip()))
+                replied_inlist[temp[0].rstrip()]=temp[1].rstrip()
+                tweet_ids[temp[0].rstrip()]=temp[2].rstrip()
+                tweet_ids[temp[1].rstrip()]=temp[3].rstrip()
+            except KeyError:
+                pass    
+        f.close()
+    return tweet_ids,replied_inlist,replied_to
 
-plt.rc('font', **font)
-
-with open('../DATA/harvey_ids.csv') as inputFile:
-    for row in inputFile:
-        harvey_id[row.rstrip()]=row.rstrip()
-    f = open('../DATA/harvey_replies.csv')
-    next(f)
-    for row in f:
-        temp=row.split(',')
+def trimTerminators(replied_to,replied_inlist):
+    '''
+    If replied to tweet exists outside of the dataset, we terminate them by having the edge to the same node
+    :param replied_to: List of edges between 2 users
+    :param replied_inlist: Dict of all ids of users that replied
+    :return replied_to: List of tuples representing edges.
+    '''
+    for row in range(len(replied_to)):
+        x,y = replied_to[row]
         try:
-            harvey_id[temp[1].rstrip()]
-            replied_to.append((temp[0].rstrip(),temp[1].rstrip()))
-            replied_inlist[temp[0].rstrip()]=temp[1].rstrip()
-            harvey_id[temp[0].rstrip()]=temp[2].rstrip()
-            harvey_id[temp[1].rstrip()]=temp[3].rstrip()
+            replied_inlist[y]
         except KeyError:
-            pass
+            replied_to.append((y,y))    
+    #return replied_to
+
+def buildReplyTree(replied_to,tweet_ids):
+    '''
+    Builds a list of dicts which represents reply trees
+    :param replied_to: list of tuples representing edges in the reply tree
+    :param tweet_ids: List of all tweet ids
+    :return forest: List of all reply trees
+    '''
+    nodes = {}
+    for i in tweet_ids:
+        nodes[i] = { 'id': i }
+    forest = []
+    for i in replied_to:
+        id, parent_id = i
         
-    f.close()
-for row in range(len(replied_to)):
-    x,y = replied_to[row]
-    try:
-        replied_inlist[y]
-    except KeyError:
-        replied_to.append((y,y))
-nodes = {}
-for i in harvey_id:
-    nodes[i] = { 'id': i }
-forest = []
-for i in replied_to:
-    id, parent_id = i
-    node = nodes[id]
-    if id == parent_id:
-        forest.append(node)
-    else:
-        parent = nodes[parent_id]
-        if not 'id_children' in parent:
-            parent['id_children'] = []
-        children = parent['id_children']
-        children.append(node)
-for row in forest:
-    try:
-        row["id_children"]
-    except KeyError:
-        forest.remove(row)
-forest.sort(key=lambda x:len(x["id_children"]),reverse=1)
-forest = list({v['id']:v for v in forest}.values())
-print(len(forest))
-print(forest.count(forest[0]))
-'''
-with open('../DATA/logfile.txt','w') as logFile:
+        node = nodes[id]
+        if id == parent_id:
+            forest.append(node)
+        else:
+            parent = nodes[parent_id]
+            if not 'id_children' in parent:
+                parent['id_children'] = []
+            children = parent['id_children']
+            children.append(node)
     for row in forest:
-        if len(row)>1:
-            pprint.pprint(row, logFile)
-            
+        try:
+            row["id_children"]
+        except KeyError:
+            forest.remove(row)
+    forest.sort(key=lambda x:len(x["id_children"]),reverse=1)
+    forest = list({v['id']:v for v in forest}.values())
+    print(len(forest))
+    print(forest.count(forest[0]))
+    return forest
 
-'''
-fig_size = plt.rcParams["figure.figsize"]
-fig_size[0] = 6
-fig_size[1] = 6
-plt.rcParams["figure.figsize"] = fig_size
+def createLog(forest):
+    '''
+    Prints the forest as a log file
+    :param forest: List of dicts of reply trees
+    '''
+    with open('../DATA/logfile.txt','w') as logFile:
+        for row in forest:
+            if len(row)>1:
+                pprint.pprint(row, logFile)
+                
 
-for row in range(len(forest)) :
-    test = 1
-    #print(row)
-    g = nx.Graph()
-    traverse(g,forest[row],harvey_id)
 
-    d=dict(g.degree)
-    nx.draw_networkx(g, pos = nx.fruchterman_reingold_layout(g),node_size=[v * 2 for v in d.values()],font_size=1,width =0.08)
-    plt.savefig('../DATA/graph'+str(row)+'.png' ,dpi = 1200)
-    print(row)
-    g.clear()
-    plt.clf()
+def plotGraphs(forest,tweet_ids):
+    '''
+    Plots all graphs in the forest.
+    :param forest: List of reply trees
+    :param tweet_ids: Dict of all tweet ids
+    '''
+    fig_size = plt.rcParams["figure.figsize"]
+    fig_size[0] = 6
+    fig_size[1] = 6
+    plt.rcParams["figure.figsize"] = fig_size
+    font = {'family' : 'normal',
+            'size'   : 1}
 
-cnx.close()
+    plt.rc('font', **font)
+    for row in range(len(forest)) :
+        test = 1
+        #print(row)
+        g = nx.Graph()
+        traverse(g,forest[row],tweet_ids)
+
+        d=dict(g.degree)
+        nx.draw_networkx(g, pos = nx.fruchterman_reingold_layout(g),node_size=[v * 2 for v in d.values()],font_size=1,width =0.08)
+        plt.savefig('../DATA/graph'+str(row)+'.png' ,dpi = 1200)
+        print(row)
+        g.clear()
+        plt.clf()
+
+
 
 if __name__ == "__main__":
     '''
     Usage: python build-tree.py <path to file of ids> <path to file of replies>
+    Original location: '../DATA/harvey_ids.csv'  '../DATA/harvey_replies.csv'
+
 
     '''
-    idfile = sys.argv[1]
-    replyfile = sys.argv[2]
-    
+    idFile = sys.argv[1]
+    replyFile = sys.argv[2]
+
+    cnx = mysql.connector.connect(user='root', password='password',
+                              host='127.0.0.1',
+                              database='twitter')
+
+    cursor = cnx.cursor()
+    tweet_ids,replied_inlist,replied_to=readFile(idFile,replyFile)
+    trimTerminators(replied_to,replied_inlist)
+    forest = buildReplyTree(replied_to,tweet_ids)
+    plotGraphs(forest,tweet_ids)
+    cnx.close()
--- readFilecsv.py
+++ readFilecsv.py
@@ -1,10 +1,10 @@
 import json
 import csv
-with open("../DATA/irma.json",'r',encoding='utf-8') as json_file:
+with open("../DATA/harvey2days.json",'r',encoding='utf-8') as json_file:
     dictList=[]
     idDict={}
     
-    f= open("../DATA/irma_cleaned.csv","w+",encoding='utf-8',newline='')
+    f= open("../DATA/harvey2daysoutput.csv","w+",encoding='utf-8',newline='')
     #f.write("{")
     for line in json_file:
 
@@ -23,16 +23,16 @@ with open("../DATA/irma.json",'r',encoding='utf-8') as json_file:
                 
                 newDict['in_reply_to_screen_name']=data['in_reply_to_screen_name']
                 if newDict['in_reply_to_screen_name']:
-                    newDict['in_reply_to_screen_name']=newDict['in_reply_to_screen_name'].encode(encoding='UTF-8',errors='ignore')  
+                    newDict['in_reply_to_screen_name']=newDict['in_reply_to_screen_name']#.encode(encoding='UTF-8',errors='ignore')  
                 #if len(data['entities']['hashtags'])>0:
 
                 #    newDict['hashtags']=data['entities']['hashtags'][0]['text']
                 newDict['user_id_str']=data['user']['id_str']
                 newDict['user_followers_count']=data['user']['followers_count']
-                newDict['user_name']=data['user']['name'].encode(encoding='UTF-8',errors='ignore')
+                newDict['user_name']=data['user']['name']#.encode(encoding='UTF-8',errors='ignore')
                 newDict['retweet_count']=data['retweet_count']
                 newDict['favorite_count']=data['favorite_count']
-                newDict['full_text']=(data['full_text'].replace('\n','')).encode(encoding='UTF-8',errors='ignore')
+                newDict['full_text']=(data['full_text'].replace('\n',''))#.encode(encoding='UTF-8',errors='ignore')
                 dictList.append(newDict)     
                 #f.write(json.dumps(newDict))
                 #f.write(",\n")
